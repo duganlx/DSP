ea42ef117b22123fd4e807978e57255782cde391
duganlx <840797783@qq.com>
2019-12-22 16:47:24 +0800

    arrange

--- README.md
+++ README.md
@@ -3,38 +3,53 @@ DSP技术及应用
 
 ## 目录说明
 
-### work01
+### demo01
 > 描述：1602字符液晶显示    
 > 功能：
 > - 水平波动显示字符串
 > - 带光标显示随机算术式
 > - 全码表字符显示
-> - CGRAM自定义字符显示
+> - CGRAM自定义字符显示   
 > 代码分析，可以参看[blog](https://blog.csdn.net/qq_40626497/article/details/103609525)
 
-### work02
+### demo02
 > 描述：ADC0832模数转换和LCD显示   
-> 功能：显示电压值并用图标显示百分比
+> 功能：显示电压值并用图标显示百分比   
+> 代码分析，可以参看[blog](https://blog.csdn.net/qq_40626497/article/details/103615232)
 
-### work03
+### demo03
 > 描述：ADC0809模数转换与显示   
-> 功能：用电阻阻值大小控制，并用七段数码管显示0~500的数  
+> 功能：用电阻阻值大小控制，并用七段数码管显示0~500的数   
+> 代码分析，可以参看[blog](https://blog.csdn.net/qq_40626497/article/details/103650705)
 
-### work04
+### demo04
 > 描述：1-Wire总线温度传感器DS18B20应用测试   
-> 功能：将温度传感器中的数值在LCD显示出来  
+> 功能：将温度传感器中的数值在LCD显示出来    
+> 代码分析，可以参看[blog](https://blog.csdn.net/qq_40626497/article/details/103650891)
 
-### work05
+### demo05
 > 描述：正反转可控的步进电机   
-> 功能：通过按钮控制电机顺时针或逆时针旋转      
+> 功能：通过按钮控制电机顺时针或逆时针旋转       
+> 代码分析，可以参看[blog](https://blog.csdn.net/qq_40626497/article/details/103651557)
 
-### work06
+### demo06
 > 描述：温度控制直流电机转速   
-> 功能：将温度传感器中的数值在LCD显示出来并且在不同问题控制电机顺时针或逆时针旋转      
+> 功能：将温度传感器中的数值在LCD显示出来并且在不同问题控制电机顺时针或逆时针旋转    
+> 代码分析，可以参看[blog](https://blog.csdn.net/qq_40626497/article/details/103653677)  
 
-### work07
+### demo07
 > 描述：数码管显示的GP2D12仿真测距警报器   
-> 功能：将红外传感器感知距离用LCD显示，在小于30cm时，发出报警
+> 功能：将红外传感器感知距离用LCD显示，在小于30cm时，发出报警   
+> 代码分析，可以参看[blog](https://blog.csdn.net/qq_40626497/article/details/103654129)  
 
-### work08
-> 1125综合实训
+### demo08
+> 1125综合实训   
+> 代码分析，可以参看[blog](https://blog.csdn.net/qq_40626497/article/details/103654386)  
+
+### 编程素材
+> CCS编写代码相关文件
+
+### HelloDSP
+> 学习创建CCS工程   
+> 相关：
+> - [利用CCS创建工程](https://blog.csdn.net/qq_40626497/article/details/103564550)
---
+++ demo06/MainX.c
@@ -0,0 +1,95 @@
+#include<reg51.h>
+#include<intrins.h>
+#include<string.h>
+#include<stdlib.h>
+#include<stdio.h>
+
+#define INT8U unsigned char
+#define INT16U unsigned int
+
+extern void delay_ms(INT16U x);
+extern void LCD_Initialize();
+extern void LCD_String(INT8U r,INT8U c,INT8U *str);
+extern INT8U Temp_Value[];
+extern void delayx(INT16U x);
+extern INT8U Read_temperature();
+
+sbit MA=P1^0;//直流电机A端 
+sbit MB=P1^1;//直流电机B端 
+sbit PWM=P1^2;//pulse width modification 脉冲宽度调制 功能调速
+
+static int count = 0;
+float tempv;
+INT8U buff[17];
+
+void T0_interrupt() interrupt 1 //INT0 0, T0 1, INT1 2, T1 3, Serial T 4 
+{
+	count++;
+	TL0 = ((INT16U)(65536-(INT16U)(11.0592/12*500)))%256; //t0 low delay->500us
+	TH0 = ((INT16U)(65536-(INT16U)(11.0592/12*500)))%256; //t0 high
+	if(count==10)		//增加延时 -> 50ms
+	{
+		count=0;
+		
+		if(Read_temperature()==1) //顺序程序设计会把执行语句作为延时
+		{
+			count=0;
+			tempv=(((int)Temp_Value[1]<<8)|((int)Temp_Value[0]))*0.0625;
+			sprintf(buff,"temp:  %5.1f\xDF\x43",tempv);  
+			LCD_String(1,0,buff);
+			if(tempv>=75)
+				tempv=75;//正转75度是全速了  temp speed
+			if(tempv<=0)
+				tempv=0;//反转0度是全速了
+			if(tempv>=45)
+			{   
+				MA=1;
+				MB=0;
+				if(tempv==45)
+					PWM=0;
+				else if(tempv==75)
+					PWM=1;
+				else
+				{   
+					PWM=1;delay_ms(tempv-45); //给力
+					PWM=0;delay_ms(75-tempv);//不给力
+				}
+			}
+			else if(tempv<=10)
+			{   
+				MA=0;
+				MB=1;
+				if(tempv==10) 
+					PWM=0;
+				else if(tempv==0)  
+					PWM=1;
+				else
+				{   
+					PWM=1;delay_ms(10-tempv); //给力
+					PWM=0;delay_ms(tempv-0);//不给力
+				}
+
+			}
+			else
+			{   
+				MA=0;
+				MB=0;
+			}
+		}
+	}
+}
+
+void main()
+{ 
+	LCD_Initialize();	
+	LCD_String(0,0,"  recent temp");
+	LCD_String(1,0,"  Waiting.......");
+	Read_temperature();
+	delay_ms(1500);
+	TMOD=0x01; //定时器16位 65535
+	EA = 1; //总开关
+	ET0 = 1; //分开关
+	TR0 = 1; //定时器开关
+	
+	while(1);//避免跑飞
+}
